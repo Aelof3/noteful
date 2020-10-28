d06438659ad831e73d964e599623680c9a29e4c6
Aelof3 <folea3@gmail.com>
2020-10-28 08:02:28 -0400

    Updates

---
+++ precache-manifest.c34197f0a01dd6182c5ffefe515ba93d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779b01c13a69957a6141793d815d57be",
+    "url": "/noteful/index.html"
+  },
+  {
+    "revision": "b17f65b993b4c732b436",
+    "url": "/noteful/static/css/main.309e25b7.chunk.css"
+  },
+  {
+    "revision": "45023d29edebf4d4c42a",
+    "url": "/noteful/static/js/2.1c71f8c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/noteful/static/js/2.1c71f8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17f65b993b4c732b436",
+    "url": "/noteful/static/js/main.0f2d9ad6.chunk.js"
+  },
+  {
+    "revision": "a0894a8e1c59296c010f",
+    "url": "/noteful/static/js/runtime-main.67ad4e08.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.faa841c0e8ece3fbc9922f2a197aa98a.js"
+  "/noteful/precache-manifest.c34197f0a01dd6182c5ffefe515ba93d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noteful/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
