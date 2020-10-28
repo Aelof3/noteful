8d543aaf959ed049f14ff5db561e8cd6acb40db7
Aelof3 <folea3@gmail.com>
2020-10-28 07:55:40 -0400

    Updates

---
+++ precache-manifest.094223ff262095d3f90971f4e7e7ff09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0b0619fdf781d0fba9435d8f0e64b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b17f65b993b4c732b436",
+    "url": "/static/css/main.309e25b7.chunk.css"
+  },
+  {
+    "revision": "45023d29edebf4d4c42a",
+    "url": "/static/js/2.1c71f8c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c71f8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17f65b993b4c732b436",
+    "url": "/static/js/main.0f2d9ad6.chunk.js"
+  },
+  {
+    "revision": "5e76467357d55565e73a",
+    "url": "/static/js/runtime-main.93766b80.js"
+  }
+]);
